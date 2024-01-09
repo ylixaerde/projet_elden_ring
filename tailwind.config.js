77a79a32cd6fe4f4b4028457791b1e38e2c71597
ylixaerde <yohan.lixaerde@francoisdesales.be>
2024-01-09 10:45:12 +0100

    version 0 : initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'elden-ring-wallpaper': "url('../img/elden-ring-wallpaper-1280-720.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
